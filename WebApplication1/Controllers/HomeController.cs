f2bcdef694b89780a59eba4eefc449b67628fc9b
UMMANCODE <ummanmm@code.edu.az>
2024-04-16 01:06:54 +0400

    Files added

---
+++ WebApplication1/Controllers/HomeController.cs
@@ -0,0 +1,31 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using WebApplication1.Models;
+
+namespace WebApplication1.Controllers
+{
+    public class HomeController : Controller
+    {
+        public readonly List<Product> products;
+
+        public HomeController() {
+            products = new List<Product> {
+                new Product { Id = 1, Name = "Product 1", Price = 8 },
+                new Product { Id = 2, Name = "Product 2", Price = 5 },
+                new Product { Id = 3, Name = "Product 3", Price = 30 },
+                new Product { Id = 4, Name = "Product 4", Price = 40 },
+                new Product { Id = 5, Name = "Product 5", Price = 15 }
+            };
+        }
+        public IActionResult Index()
+        {
+            ViewBag.Products = products;
+            return View();
+        }
+
+        public IActionResult Detail(int id) {
+            var product = products.FirstOrDefault(x => x.Id == id);
+            ViewBag.Product = product;
+            return View();
+        }
+    }
+}
---
+++ WebApplication1/Models/Product.cs
@@ -0,0 +1,7 @@
+﻿namespace WebApplication1.Models {
+    public class Product {
+        public int Id { get; set; }
+        public string? Name { get; set; }
+        public double Price { get; set; }
+    }
+}
